b624d230c59793ca1080e94a0905600b95b30c12
jys923 <jys923@gmail.com>
2024-09-09 16:36:21 +0900

    i2C pass

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 // I2C 설정
 #define USE_I2C
 #define I2C_DRIVER I2CD0
-#define I2C0_SDA_PIN   GP4
-#define I2C0_SCL_PIN   GP5
+#define I2C1_SDA_PIN   GP4
+#define I2C1_SCL_PIN   GP5
 
 //#define POINTING_DEVICE_DEBUG
 
